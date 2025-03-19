966290f4aeb3c7e7bf5c4984a99737b45389497f
darapaulmakwe <dpaulmakwe@gmail.com>
2025-03-19 15:30:45 -0400

    Mock final version of Main menu Layout

--- app/src/main/java/com/jupiterjam/Mainmenu.java
+++ app/src/main/java/com/jupiterjam/Mainmenu.java
@@ -1,4 +1,21 @@
 package com.jupiterjam;
 
-public class Mainmenu {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mainmenu extends AppCompatActivity  {
+
+    public void openPlayMenu(View myView){
+        Button playButton = (Button) myView;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_menu);
+    }
 }
