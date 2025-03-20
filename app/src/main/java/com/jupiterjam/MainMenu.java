b50aadb99566f0f701e4aeafb6ca310ecc299d25
darapaulmakwe <dpaulmakwe@gmail.com>
2025-03-20 11:37:47 -0400

    Modified Main menu layout, created the layout for play menu and profile menu, Made the play button and profile button lead to their respective layouts.

---
+++ app/src/main/java/com/jupiterjam/MainMenu.java
@@ -0,0 +1,30 @@
+package com.jupiterjam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_menu);
+    }
+
+    public void openPlayMenu(View myView){
+        startActivity(new Intent(this,PlayMenu.class));
+        finish();
+
+    }
+    public void openProfileMenu(View myView){
+        startActivity(new Intent(this,ProfileMenu.class));
+        finish();
+
+    }
+
+}
---
+++ app/src/main/java/com/jupiterjam/PlayMenu.java
@@ -0,0 +1,15 @@
+package com.jupiterjam;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.play_menu);
+    }
+}
