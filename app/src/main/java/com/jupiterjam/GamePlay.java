76839e786f1916c7ea6195f97d688da34896635d
Ajibade-Daniel <159869974+Ajibade-Daniel@users.noreply.github.com>
2025-03-22 12:57:00 -0600

    Started on the gameplay

---
+++ app/src/main/java/com/jupiterjam/GamePlay.java
@@ -0,0 +1,10 @@
+package com.jupiterjam;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GamePlay extends AppCompatActivity {
+ @Override
+    protected void onCreate(Bundle savedInstanceState){super.onCreate(savedInstanceState);}
+}
---
+++ app/src/main/java/com/jupiterjam/GameView.java
@@ -0,0 +1,53 @@
+package com.jupiterjam;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable{
+
+    private Thread thread;
+    private boolean isPlaying;
+    public GameView(Context context){
+        super(context);
+    }
+
+    @Override
+    public void run() {
+        while(isPlaying){
+
+            update();
+            draw();
+            sleep();
+
+        }
+    }
+    private void update(){
+
+    }
+
+    private void draw(){
+
+    }
+    private void sleep(){
+        try {
+            Thread.sleep(17);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void resume(){
+        isPlaying = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void pause(){
+        try {
+            isPlaying = false;
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        ;
+    }
+}
