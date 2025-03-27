6686f7303bd3d613fdc66f5c1194b18a11ba9e2d
Madison Allen <mallen5@ualberta.ca>
2025-03-27 13:20:11 -0600

    Fixed Profile and Theme menu
    Put theme menu functionality back into profile menu. made the textview in theme dynamic to what theme is selected

--- app/src/main/java/com/jupiterjam/Customization.java
+++ app/src/main/java/com/jupiterjam/Customization.java
@@ -1,25 +1,16 @@
 package com.jupiterjam;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Customization extends AppCompatActivity {
-
-    TextView currentTheme;
     public String userName;
     public String themeName;
     public String themeImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
-        currentTheme = findViewById(R.id.currentTheme);
-
-        setContentView(R.layout.theme_menu);
     }
 
     public Customization(String userName, String themeName, String themeImage){
@@ -46,37 +37,6 @@ public class Customization extends AppCompatActivity {
 
     public String getThemeImage(){
         return this.themeImage;
-    }
-    //    public void changeTheme(View myView){
-//
-//        if (myView.getId() == R.id.themeButton1) {
-//            userPreferences.setTheme("jupiter");
-//            currentTheme.setText(userPreferences.getThemeName());
-//        }
-//        else if (myView.getId() == R.id.themeButton2) {
-//            userPreferences.setTheme("earth");
-//            currentTheme.setText(userPreferences.getThemeName());
-//        }
-//        else if (myView.getId() == R.id.themeButton3) {
-//            userPreferences.setTheme("mars");
-//            currentTheme.setText(userPreferences.getThemeName());
-//        }
-//        else if (myView.getId() == R.id.themeButton4) {
-//            userPreferences.setTheme("neptune");
-//            currentTheme.setText(userPreferences.getThemeName());
-//        }
-//        else if (myView.getId() == R.id.themeButton5) {
-//            userPreferences.setTheme("saturn");
-//            currentTheme.setText(userPreferences.getThemeName());
-//        }
-//        else if (myView.getId() == R.id.themeButton6) {
-//            userPreferences.setTheme("moon");
-//            currentTheme.setText(userPreferences.getThemeName());
-//        }
-//    }
-    public void backToProfile(View myView) {
-        startActivity(new Intent(this,ProfileMenu.class));
-        finish();
     }
 
 }
--- app/src/main/java/com/jupiterjam/ProfileMenu.java
+++ app/src/main/java/com/jupiterjam/ProfileMenu.java
@@ -4,19 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileMenu extends AppCompatActivity {
 
     EditText userName;
+    TextView currentTheme;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        userName = findViewById(R.id.usernameInput);
-
         setContentView(R.layout.profile_menu);
+
+        userName = findViewById(R.id.usernameInput);
     }
 
     Customization userPreferences = new Customization("","jupiter","jupiter_ship.png");
@@ -31,10 +33,44 @@ public class ProfileMenu extends AppCompatActivity {
     }
 
     public void openThemeMenu(View myView){
-        startActivity(new Intent(this,Customization.class));
-        finish();
+        setContentView(R.layout.theme_menu);
+
+        currentTheme = findViewById(R.id.currentTheme);
+
     }
 
+    public void backToProfile(View myView) {
+        setContentView(R.layout.profile_menu);
+    }
+
+    //TODO: enumerate buttons and use switch statement
+    public void changeTheme(View myView){
+
+        if (myView.getId() == R.id.themeButton1) {
+            userPreferences.setTheme("jupiter");
+            currentTheme.setText(userPreferences.getThemeName());
+        }
+        else if (myView.getId() == R.id.themeButton2) {
+            userPreferences.setTheme("earth");
+            currentTheme.setText(userPreferences.getThemeName());
+        }
+        else if (myView.getId() == R.id.themeButton3) {
+            userPreferences.setTheme("mars");
+            currentTheme.setText(userPreferences.getThemeName());
+        }
+        else if (myView.getId() == R.id.themeButton4) {
+            userPreferences.setTheme("neptune");
+            currentTheme.setText(userPreferences.getThemeName());
+        }
+        else if (myView.getId() == R.id.themeButton5) {
+            userPreferences.setTheme("saturn");
+            currentTheme.setText(userPreferences.getThemeName());
+        }
+        else if (myView.getId() == R.id.themeButton6) {
+            userPreferences.setTheme("moon");
+            currentTheme.setText(userPreferences.getThemeName());
+        }
+    }
     /*public int getWins(Player user){
         return user.wins;
     }
