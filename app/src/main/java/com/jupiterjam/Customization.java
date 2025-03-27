35e204cd166e2573a2658bdba9d38fdeb0bd5577
Madison Allen <mallen5@ualberta.ca>
2025-03-26 22:28:48 -0600

    Updated Profile Menu options and Created theme menu
    Made option to update username in profile menu. Made space to display wins and loses to be tracked in player.  Created theme page and buttons to apply different theme customizations that will be added to the new customization class. NOTE: Theme menu crashes the app currently.

--- app/src/main/java/com/jupiterjam/Customization.java
+++ app/src/main/java/com/jupiterjam/Customization.java
@@ -1,29 +1,82 @@
 package com.jupiterjam;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import androidx.activity.EdgeToEdge;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Customization extends AppCompatActivity {
-    private string name;
-    private string theme;
-    private int wins;
-    private int loses;
 
-    private Player(string name, int health, string theme, int wins, int loses ){
-        this.name = name;
-        this.theme = theme;
+    TextView currentTheme;
+    public String userName;
+    public String themeName;
+    public String themeImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
 
+        currentTheme = findViewById(R.id.currentTheme);
+
+        setContentView(R.layout.theme_menu);
     }
-    private void setName(Player player){
 
+    public Customization(String userName, String themeName, String themeImage){
+        this.userName = userName;
+        this.themeName = themeName;
+        this.themeImage = themeImage;
+    }
+    public void setName(String name){
+        this.userName = name;
     }
 
-    public void setTheme(string theme){
-        this.theme = theme;
+    public String getName(){
+        return this.userName;
     }
+
+    public void setTheme(String theme){
+        this.themeName = theme;
+        this.themeImage = theme +"_ship.png";
+    }
+
+    public String getThemeName(){
+        return this.themeName;
+    }
+
+    public String getThemeImage(){
+        return this.themeImage;
+    }
+    //    public void changeTheme(View myView){
+//
+//        if (myView.getId() == R.id.themeButton1) {
+//            userPreferences.setTheme("jupiter");
+//            currentTheme.setText(userPreferences.getThemeName());
+//        }
+//        else if (myView.getId() == R.id.themeButton2) {
+//            userPreferences.setTheme("earth");
+//            currentTheme.setText(userPreferences.getThemeName());
+//        }
+//        else if (myView.getId() == R.id.themeButton3) {
+//            userPreferences.setTheme("mars");
+//            currentTheme.setText(userPreferences.getThemeName());
+//        }
+//        else if (myView.getId() == R.id.themeButton4) {
+//            userPreferences.setTheme("neptune");
+//            currentTheme.setText(userPreferences.getThemeName());
+//        }
+//        else if (myView.getId() == R.id.themeButton5) {
+//            userPreferences.setTheme("saturn");
+//            currentTheme.setText(userPreferences.getThemeName());
+//        }
+//        else if (myView.getId() == R.id.themeButton6) {
+//            userPreferences.setTheme("moon");
+//            currentTheme.setText(userPreferences.getThemeName());
+//        }
+//    }
+    public void backToProfile(View myView) {
+        startActivity(new Intent(this,ProfileMenu.class));
+        finish();
+    }
+
 }
--- app/src/main/java/com/jupiterjam/ProfileMenu.java
+++ app/src/main/java/com/jupiterjam/ProfileMenu.java
@@ -3,21 +3,43 @@ package com.jupiterjam;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileMenu extends AppCompatActivity {
+
+    EditText userName;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
+        userName = findViewById(R.id.usernameInput);
+
         setContentView(R.layout.profile_menu);
     }
+
+    Customization userPreferences = new Customization("","jupiter","jupiter_ship.png");
 
     public void revert(View myView){
         startActivity(new Intent(this,MainMenu.class));
         finish();
-
     }
 
+    public void changeUserName(View myView){
+        userPreferences.setName(userName.getText().toString());
+    }
+
+    public void openThemeMenu(View myView){
+        startActivity(new Intent(this,Customization.class));
+        finish();
+    }
+
+    /*public int getWins(Player user){
+        return user.wins;
+    }
+
+    public int getLoses(Player user){
+        return user.loses;
+    }*/
 }
