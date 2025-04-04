51d106225a0e1211ca36ba3b40b44503e6cd4585
darapaulmakwe <dpaulmakwe@gmail.com>
2025-04-04 12:58:54 -0400

    Implemented the pause Menu. I need to constrain it in the desired position and also fix a minor bug but aside from that it works perfectly

--- app/src/main/java/com/jupiterjam/GamePlay.java
+++ app/src/main/java/com/jupiterjam/GamePlay.java
@@ -1,17 +1,30 @@
 package com.jupiterjam;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GamePlay extends AppCompatActivity {
     // Define Player Object
     private Player player;
+    private boolean isPaused = false;
+
+    //Define Pause menu things
+    ImageView pauseMenu;
+    ImageButton pauseBtn;
+    TextView pauseMenutext;
+    public Button resumeButton;
+    private Button homeButton;
 
     //Define sensor variables
     private SensorManager mSensorManager;
@@ -48,6 +61,27 @@ public class GamePlay extends AppCompatActivity {
      // Initialize player sprite
      ImageView playerSprite = findViewById(R.id.rocket);
      player = new Player(playerSprite);
+
+     //Initialize pause menu buttons
+     pauseMenu = findViewById(R.id.pauseMenu);
+     resumeButton = findViewById(R.id.resumeButton);
+     homeButton = findViewById(R.id.homeButton);
+     pauseBtn = findViewById(R.id.pauseButton);
+     pauseMenutext = findViewById(R.id.textView);
+
+     pauseBtn.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             pauseGame();
+         }
+     });
+     resumeButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             resumeGame();
+         }
+     });
+
  }
 
     //Generate a sensor event listener for the application
@@ -60,6 +94,29 @@ public class GamePlay extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(sensorEventListener);
+    }
+
+    //Toggle functions for Pause Menu
+    public void resumeGame(){
+        pauseMenu.setVisibility(View.GONE);
+        resumeButton.setVisibility(View.GONE);
+        homeButton.setVisibility(View.GONE);
+        pauseMenutext.setVisibility(View.GONE);
+        onResume();
+        isPaused = false;
+    }
+    public void pauseGame(){
+        pauseMenu.setVisibility(View.VISIBLE);
+        resumeButton.setVisibility(View.VISIBLE);
+        homeButton.setVisibility(View.VISIBLE);
+        pauseMenutext.setVisibility(View.VISIBLE);
+        onPause();
+        isPaused = true;
+    }
+
+    public void revertToMainMenu(View myView){
+        startActivity(new Intent(this,MainMenu.class));
+        finish();
     }
 
 }
